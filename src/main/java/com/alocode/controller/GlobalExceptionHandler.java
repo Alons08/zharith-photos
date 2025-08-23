13cee6d324dad769ff6e1f73d82fc3f0c200fdce
Your Name <you@example.com>
2025-08-23 17:23:40 -0500

    Eliminar importación no utilizada en GlobalExceptionHandler

--- src/main/java/com/alocode/controller/GlobalExceptionHandler.java
+++ src/main/java/com/alocode/controller/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
